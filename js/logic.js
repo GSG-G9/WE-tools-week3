5b45abee4ef47962066bb1bf5a3844139bcf003d
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-11-11 12:15:12 +0200

    change filter relate #10

--- js/logic.js
+++ js/logic.js
@@ -3,8 +3,8 @@ const searchCity = (cityName, AppData) => {
 	if (cityName === "") {
 		return "city name is invalid";
 	}
-	const foundData = AppData.filter(
-		(obj) => obj.country === cityName.toLowerCase().trim()
+	const foundData = AppData.filter((obj) =>
+		obj.country.toLowerCase().includes(cityName.toLowerCase().trim())
 	);
 	if (foundData.length === 0) {
 		return "city not found";
