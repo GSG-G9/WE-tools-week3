1a1ce98d01f1910283aa7d2087368818f83ac5e4
Muhammad Alabadsa <moh22389@gmail.com>
2020-11-12 10:21:19 +0200

    Fix module not defined Relates #31

--- js/logic.js
+++ js/logic.js
@@ -1,23 +1,25 @@
 let searchCity = (cityName, AppData) => {
-	if (cityName === "") {
-		return "city name is invalid";
-	}
-	const foundData = AppData.filter((obj) =>
-		obj.cityName.toLowerCase().includes(cityName.toLowerCase().trim())
-	);
-	if (foundData.length === 0) {
-		return "city not found";
-	}
-	return foundData;
+  if (cityName === "") {
+    return "city name is invalid";
+  }
+  const foundData = AppData.filter((obj) =>
+    obj.cityName.toLowerCase().includes(cityName.toLowerCase().trim())
+  );
+  if (foundData.length === 0) {
+    return "city not found";
+  }
+  return foundData;
 };
 
 function editObject(newObj, objectArrays) {
-	const originalObject = objectArrays.filter((obj) => obj.id === newObj.id)[0];
-	Object.keys(newObj).forEach((key) => (originalObject[key] = newObj[key]));
-	return [
-		originalObject,
-		...objectArrays.filter((obj) => obj.id !== newObj.id),
-	];
+  const originalObject = objectArrays.filter((obj) => obj.id === newObj.id)[0];
+  Object.keys(newObj).forEach((key) => (originalObject[key] = newObj[key]));
+  return [
+    originalObject,
+    ...objectArrays.filter((obj) => obj.id !== newObj.id),
+  ];
 }
 
-//module.exports = { searchCity, editObject };
+if (typeof module !== "undefined") {
+  module.exports = { searchCity, editObject };
+}
